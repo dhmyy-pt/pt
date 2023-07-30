c1f05d85a82c9cf158d223f8741e68e1308a45f7
Kicey <kiceyscream@qq.com>
2023-07-30 20:03:47 +0800

    Fix copy script.

--- docker/copy.sh
+++ docker/copy.sh
@@ -9,12 +9,12 @@ usage() {
 
 # copy sql
 echo "begin copy sql "
-cp ../sql/ry_20230223.sql ./mysql/db
+cp ../sql/ry_20230706.sql ./mysql/db
 cp ../sql/ry_config_20220929.sql ./mysql/db
 
 # copy html
 echo "begin copy html "
-cp -r ../ruoyi-ui/dist/** ./nginx/html/dist
+cp -r ../react-ui/dist/** ./nginx/html/dist
 
 
 # copy jar
